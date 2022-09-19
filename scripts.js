5a31c55222827ee5bedbce03699a072e66bb6f47
Nabil <holanabilkhalid@gmail.com>
2022-09-19 22:29:53 +0800

    Added mouseenter event to change color of the grids

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Create 16X16 grid
 const mainDiv = document.querySelector("#main");
 
 for (let x = 0; x < 16; x++ ){
@@ -7,7 +8,17 @@ for (let x = 0; x < 16; x++ ){
 
     for (let i = 0; i < 16; i++){
         const horizontalDiv = document.createElement("div");
+        horizontalDiv.className = "hdiv"
         horizontalDiv.setAttribute("style", " border: 1px solid green; width:37.5px; height: 37.5px; ")
         verticalDiv.appendChild(horizontalDiv);
     }
 }
+
+// Adding an event when mouse entering and change colour of the grid
+const smallGrid = document.querySelectorAll(".hdiv");
+smallGrid.forEach(grid => {
+    grid.addEventListener("mouseenter", changeColor);
+    function changeColor(){
+            grid.style.background = "grey";       
+    }
+})
