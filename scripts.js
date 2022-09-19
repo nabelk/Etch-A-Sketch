871cc9aa9ceb61dcff67e0c3dff378a3c37d511f
Nabil <holanabilkhalid@gmail.com>
2022-09-19 16:44:24 +0800

    Add loop/dom to create 16X16 grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 const mainDiv = document.querySelector("#main");
-for (let i = 0; i < 16; i++){
-    const createDiv = document.createElement("div");
-    createDiv.style.border = "2px solid red"
-    mainDiv.appendChild(createDiv);
+
+for (let x = 0; x < 16; x++ ){
+    const verticalDiv = document.createElement("div");
+    verticalDiv.setAttribute("style", "height: auto;  width: auto; display:flex ")
+    mainDiv.appendChild(verticalDiv);
+
+    for (let i = 0; i < 16; i++){
+        const horizontalDiv = document.createElement("div");
+        horizontalDiv.setAttribute("style", " border: 1px solid green; width:37.5px; height: 37.5px; ")
+        verticalDiv.appendChild(horizontalDiv);
+    }
 }
